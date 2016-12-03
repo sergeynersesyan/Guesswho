d9a28234a4abf45d48119d87c83051f399d67ee9
Arsen <arsen.asulyan.1991@gmail.com>
2016-12-03 18:51:46 +0400

    nothing1

--- app/src/main/java/com/orig/guesswho/QuestionHelper.java
+++ app/src/main/java/com/orig/guesswho/QuestionHelper.java
@@ -88,19 +88,51 @@ public class QuestionHelper {
         questionStringIDs.put("trialAnswers3", R.array.trialAnswers3);
         questionStringIDs.put("trialAnswer3", R.string.trialAnswer3);
 
-        packageResourceIDs.put("package2UID", R.string.package2UID);
+        packageResourceIDs.put("package2UID", 2);
         packageResourceIDs.put("package2title", R.string.package2title);
         packageResourceIDs.put("package2description", R.string.package2description);
         packageResourceIDs.put("package2questionCount", R.integer.package2questionCount);
-        questionStringIDs.put("kingQuestion1", R.array.kingQuestion1);
-        questionStringIDs.put("kingAnswers1", R.array.kingAnswers1);
-        questionStringIDs.put("kingAnswer1", R.string.kingAnswer1);
-        questionStringIDs.put("kingQuestion2", R.array.kingQuestion2);
-        questionStringIDs.put("kingAnswers2", R.array.kingAnswers2);
-        questionStringIDs.put("kingAnswer2", R.string.kingAnswer2);
-        questionStringIDs.put("kingQuestion3", R.array.kingQuestion3);
-        questionStringIDs.put("kingAnswers3", R.array.kingAnswers3);
-        questionStringIDs.put("kingAnswer3", R.string.kingAnswer3);
+        questionStringIDs.put("animalsQuestion1", R.array.animalsQuestion1);
+        questionStringIDs.put("animalsAnswers1", R.array.animalsAnswers1);
+        questionStringIDs.put("animalsAnswer1", R.string.animalsAnswer1);
+        questionStringIDs.put("animalsQuestion2", R.array.animalsQuestion2);
+        questionStringIDs.put("animalsAnswers2", R.array.animalsAnswers2);
+        questionStringIDs.put("animalsAnswer2", R.string.animalsAnswer2);
+        questionStringIDs.put("animalsQuestion3", R.array.animalsQuestion3);
+        questionStringIDs.put("animalsAnswers3", R.array.animalsAnswers3);
+        questionStringIDs.put("animalsAnswer3", R.string.animalsAnswer3);
+        questionStringIDs.put("animalsQuestion4", R.array.animalsQuestion4);
+        questionStringIDs.put("animalsAnswers4", R.array.animalsAnswers4);
+        questionStringIDs.put("animalsAnswer4", R.string.animalsAnswer4);
+        questionStringIDs.put("animalsQuestion5", R.array.animalsQuestion5);
+        questionStringIDs.put("animalsAnswers5", R.array.animalsAnswers5);
+        questionStringIDs.put("animalsAnswer5", R.string.animalsAnswer5);
+        questionStringIDs.put("animalsQuestion6", R.array.animalsQuestion6);
+        questionStringIDs.put("animalsAnswers6", R.array.animalsAnswers6);
+        questionStringIDs.put("animalsAnswer6", R.string.animalsAnswer6);
+        questionStringIDs.put("animalsQuestion7", R.array.animalsQuestion7);
+        questionStringIDs.put("animalsAnswers7", R.array.animalsAnswers7);
+        questionStringIDs.put("animalsAnswer7", R.string.animalsAnswer7);
+        questionStringIDs.put("animalsQuestion8", R.array.animalsQuestion8);
+        questionStringIDs.put("animalsAnswers8", R.array.animalsAnswers8);
+        questionStringIDs.put("animalsAnswer8", R.string.animalsAnswer8);
+        questionStringIDs.put("animalsQuestion9", R.array.animalsQuestion9);
+        questionStringIDs.put("animalsAnswers9", R.array.animalsAnswers9);
+        questionStringIDs.put("animalsAnswer9", R.string.animalsAnswer9);
+        questionStringIDs.put("animalsQuestion10", R.array.animalsQuestion10);
+        questionStringIDs.put("animalsAnswers10", R.array.animalsAnswers10);
+        questionStringIDs.put("animalsAnswer10", R.string.animalsAnswer10);
+        questionStringIDs.put("animalsQuestion11", R.array.animalsQuestion11);
+        questionStringIDs.put("animalsAnswers11", R.array.animalsAnswers11);
+        questionStringIDs.put("animalsAnswer11", R.string.animalsAnswer11);
+
+
+
+
+
+
+
+
 
         packageResourceIDs.put("package3UID", R.string.package3UID);
         packageResourceIDs.put("package3title", R.string.package3title);
